dbfbecc866a25a8769b6869cb3df6b8cf023b6dd
Alhassan Abdul-Aziz <117979675+Abdul-Aziz1199@users.noreply.github.com>
2024-07-19 03:41:02 +0000

    Add files via upload
    API call project for form creation

---
+++ fetch-data.js
@@ -0,0 +1,28 @@
+async function fetchUserdata(){
+    const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+
+    const dataContainer = document.getElementById('api-data');
+    try{
+        const response = await fetch(apiUrl);
+        const users = await response.json();
+        dataContainer.innerHTML = ''; //to clear the previous text
+        const userList = document.createElement('ul');
+        //looping through for each userco
+        users.forEach(user =>{
+        const listItems = document.createElement('li');
+        listItems.textContent = user.name;  //set content to users name
+        userList.appendChild(listItems);  //append li to userList
+        
+        dataContainer.appendChild(userList);  //append userList to datacontainer
+
+
+        })
+    } catch{
+        dataContainer.textContent = 'Failed to load user data.';
+    }
+  } 
+  fetchUserdata();
+  document.addEventListener('DOMContentLoaded',fetchUserdata);
+
+
+    
